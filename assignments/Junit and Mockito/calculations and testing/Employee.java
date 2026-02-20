3ba0bd0016ae6d2b42f3b52da4b7bbe7e30fb0b6
R sai krishna <115521007+saikrishnaracharla@users.noreply.github.com>
2026-02-20 09:34:22 +0530

    Create Employee.java

---
+++ assignments/Junit and Mockito/calculations and testing/Employee.java
@@ -0,0 +1,32 @@
+package com.Calculator;
+
+public class Employee {
+	private String name;
+	private double salary;
+	private int age;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public double calculateAnnualSalary() {
+		return salary *12;
+
+	}
+	
+
+}

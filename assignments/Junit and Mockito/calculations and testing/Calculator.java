4fd4c581ae3717d69e750bcf8e58469da6717b84
R sai krishna <115521007+saikrishnaracharla@users.noreply.github.com>
2026-02-20 09:29:05 +0530

    Create Calculator.java

---
+++ assignments/Junit and Mockito/calculations and testing/Calculator.java
@@ -0,0 +1,18 @@
+package com.Calculator;
+
+public class Calculator {
+
+	  public int add(int i, int j) {
+	        return i + j;
+	    }
+	  public int sub(int a,int b) {
+		  return a-b;
+	  }
+	  public int mul(int a,int b) {
+		  return a*b;
+	  }
+	  public int div(int a,int b) {
+		  return a/b;
+	  }
+}
+

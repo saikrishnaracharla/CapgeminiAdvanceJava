0694428b6c5f21c8e5d490e11795befc16467eed
R sai krishna <115521007+saikrishnaracharla@users.noreply.github.com>
2026-02-21 16:35:02 +0530

    Create Address.java

---
+++ Assessments/crm sales management/Address.java
@@ -0,0 +1,54 @@
+package com.CrmSales;
+
+import javax.persistence.*;
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+	public Long getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(Long addressId) {
+		this.addressId = addressId;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+    
+}

dc496b3a44c7f83c80a93fc74419322d3a9c7c29
R sai krishna <115521007+saikrishnaracharla@users.noreply.github.com>
2026-02-21 16:37:00 +0530

    Create Product.java

---
+++ Assessments/crm sales management/Product.java
@@ -0,0 +1,31 @@
+package com.CrmSales;
+
+import javax.persistence.*;
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+    private String productName;
+    private double price;
+	public Long getProductId() {
+		return productId;
+	}
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+    
+}

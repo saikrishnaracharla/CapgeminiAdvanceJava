7a331866a0a73e1f2a1f2482123de76307ba3319
R sai krishna <115521007+saikrishnaracharla@users.noreply.github.com>
2026-02-28 16:29:46 +0530

    Create ShopCartApplication as Spring Boot app

---
+++ Assessments/OnlineShopCarting/ShopCartApplication.java
@@ -0,0 +1,14 @@
+package com.cg.ShopCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopCartApplication.class, args);
+		System.out.println("hello");
+	}
+
+}

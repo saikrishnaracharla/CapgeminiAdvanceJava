e6012190c552d230fdca0a84ae5bc7a379fa39d8
R sai krishna <115521007+saikrishnaracharla@users.noreply.github.com>
2026-03-07 10:55:30 +0530

    Create MyController.java

---
+++ Assessments/Cacheing/MyController.java
@@ -0,0 +1,45 @@
+package com.cg.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.cg.entities.User;
+import com.cg.services.UserService;
+
+@RestController
+public class MyController {
+
+    @Autowired
+    UserService us;
+    @PostMapping("/user")
+    public User displayUser(@RequestBody User user) {
+        return us.addUserDetails(user);
+    }
+
+    // GET ALL USERS
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return us.getAllUsers();
+    }
+    @GetMapping("/user/{id}")
+    public User getUserById(@PathVariable int id) {
+        return us.getUserById(id);
+    }
+    @PutMapping("/user/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User user) {
+        return us.updateUser(id, user.getName());
+    }
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable int id) {
+
+        boolean status = us.deleteId(id);
+
+        if (status) {
+            return "User deleted successfully";
+        } else {
+            return "User not found";
+        }
+    }
+}
